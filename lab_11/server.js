541daea5bae3e19710b4ee568806b5f5f3f77510
Abhishek Abhikshek <a.abhishek@rgu.ac.uk>
2025-01-30 11:40:42 +0000

    route

--- lab_11/server.js
+++ lab_11/server.js
@@ -2,5 +2,9 @@ var express = require('express');
 var app = express();
 app.get('/', function(req, res){
  res.send("Hello world! by express");
-});
+}); 
+app.get('/', function(req, res){
+    res.send("Hello world! by express”);
+   });
 app.listen(8080); 
+
